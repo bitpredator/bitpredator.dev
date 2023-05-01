8d8b79aa0e0ce68e8d920c2893f9f11a580bfb14
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-05-01 23:34:19 +0200

    Complete website homepage

--- assets/js/dark-mode.js
+++
@@ -1,7 +0,0 @@
-<script>
-    var toggleBtn = document.getElementById('toggle-btn');
-    toggleBtn.onclick = function() {
-        this.classList.toggle('toggle-btn--dark');
-        document.body.classList.toggle('dark-mode');
-    };
-</script>
